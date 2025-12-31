f4172e61d50262398d86629161690a4b98a41488
Aniket Ninama <aniketninama5@gmail.com>
2025-12-31 13:19:39 +0530

    update add_user.py

--- add_users.py
+++ add_users.py
@@ -1,19 +1,26 @@
 from models import User
 from flask_bcrypt import Bcrypt
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_admin(app, db):
-    bcrypt = Bcrypt(app)
-    raw_password = os.getenv("PROJECT_PASSWORD")
-    hashed_password = bcrypt.generate_password_hash(raw_password).decode('utf-8')
-
-    new_admin = User(
-        username="Admin",
-        email=os.getenv("MY_EMAIL"),
-        password_hash=hashed_password,
-        role="admin"
-    )
-
-    db.session.add(new_admin)
-    db.session.commit()
-    print("Admin created")
+    with app.app_context():
+        bcrypt = Bcrypt(app)
+        raw_password = os.getenv("PROJECT_PASSWORD")
+        hashed_password = bcrypt.generate_password_hash(raw_password).decode('utf-8')
+        existing_admin = User.query.filter_by(email=os.getenv("MY_EMAIL"), role="admin").first()
+        if not existing_admin:
+            new_admin = User(
+                username="Admin",
+                email=os.getenv("MY_EMAIL"),
+                password_hash=hashed_password,
+                role="admin"
+            )
+            db.session.add(new_admin)
+            db.session.commit()
+            print("Admin created")
+        else:
+            print("Admin already exists")
+       
