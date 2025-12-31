faa8bec45cfd8a380ee8a251c2fd0e7320737c2f
Aniket Ninama <aniketninama5@gmail.com>
2025-12-31 14:13:12 +0530

    update add_user.py

--- add_users.py
+++ add_users.py
@@ -25,23 +25,4 @@ def create_admin(app, db):
     db.session.add(new_admin)
     db.session.commit()
     print("Admin created")
-=======
-    with app.app_context():
-        bcrypt = Bcrypt(app)
-        raw_password = os.getenv("PROJECT_PASSWORD")
-        hashed_password = bcrypt.generate_password_hash(raw_password).decode('utf-8')
-        existing_admin = User.query.filter_by(email=os.getenv("MY_EMAIL"), role="admin").first()
-        if not existing_admin:
-            new_admin = User(
-                username="Admin",
-                email=os.getenv("MY_EMAIL"),
-                password_hash=hashed_password,
-                role="admin"
-            )
-            db.session.add(new_admin)
-            db.session.commit()
-            print("Admin created")
-        else:
-            print("Admin already exists")
-       
->>>>>>> f4172e61d50262398d86629161690a4b98a41488
+
